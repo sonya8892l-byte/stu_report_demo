c5eedf23d736228dd7407419e339101807721a15
Sonya Liu <sonya8892l@gmail.com>
2026-07-23 15:20:38 +0800

    enable standalone student course mode

--- 4-stu-learning/src/engine/platform-config.js
+++ 4-stu-learning/src/engine/platform-config.js
@@ -2,6 +2,6 @@ export const PLATFORM_COMPANION = Object.freeze({
   name: '絮絮',
   character: '亲切、好奇、有少年感，尊重学生的观察和试错过程',
   tone: '清晰、自然、耐心，偶尔幽默',
-  idleAsset: '/assets/video/xuxu-idle.webm',
-  talkAsset: '/assets/video/xuxu-talk.webm',
+  idleAsset: './assets/video/xuxu-idle.webm',
+  talkAsset: './assets/video/xuxu-talk.webm',
 });
